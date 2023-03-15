615bdc41b29f9eb78619d9c353394b5da9920d8e
Andrian Mihailov <amihailov@wagaia.com>
2023-03-15 12:12:35 +0100

    Bugfix: Translation

--- src/Traits/Translation.php
+++ src/Traits/Translation.php
@@ -17,7 +17,7 @@ trait Translation
 
     public function translation(string $key, string $locale = null): mixed
     {
-        if (cache('multilang')) {
+        if (Locale::multilang()) {
             $locale = $locale ?? app()->getLocale();
             if (array_key_exists($key, $this->getTranslations())) {
                 return $this->getTranslation($key, $locale);
@@ -26,21 +26,21 @@ trait Translation
         }
 
         if (str_contains($key, '.')) {
-          $arrayable = explode('.', $key);
-          $var = array_shift($arrayable);
+            $arrayable = explode('.', $key);
+            $var = array_shift($arrayable);
 
-          switch(count($arrayable)) {
-              case 1:
-                  return $this->{$var}[$arrayable[0]];
-              case 2:
-                  return $this->{$var}[$arrayable[0]][$arrayable[1]];
-              case 3:
-                  return $this->{$var}[$arrayable[0]][$arrayable[1]][$arrayable[2]];
-              case 4:
-                  return $this->{$var}[$arrayable[0]][$arrayable[1]][$arrayable[2]][$arrayable[3]];
-              case 5:
-                  return $this->{$var}[$arrayable[0]][$arrayable[1]][$arrayable[2]][$arrayable[3]][$arrayable[4]];
-          }
+            switch (count($arrayable)) {
+                case 1:
+                    return $this->{$var}[$arrayable[0]];
+                case 2:
+                    return $this->{$var}[$arrayable[0]][$arrayable[1]];
+                case 3:
+                    return $this->{$var}[$arrayable[0]][$arrayable[1]][$arrayable[2]];
+                case 4:
+                    return $this->{$var}[$arrayable[0]][$arrayable[1]][$arrayable[2]][$arrayable[3]];
+                case 5:
+                    return $this->{$var}[$arrayable[0]][$arrayable[1]][$arrayable[2]][$arrayable[3]][$arrayable[4]];
+            }
 
         }
         return $this->{$key} ?? '';
@@ -48,8 +48,8 @@ trait Translation
 
     public function translatableInput(string $name, string $locale = null): string
     {
-        if (cache('multilang')) {
-            return $name.'['.$locale ?: Locale::locale() .']';
+        if (Locale::multilang()) {
+            return $name . '[' . $locale ?: Locale::locale() . ']';
         }
 
         return $name;
@@ -57,8 +57,8 @@ trait Translation
 
     public function translatableFromRequest(string $key, string $locale = null): mixed
     {
-        if (cache('multilang')) {
-            return request($key . $locale);
+        if (Locale::multilang()) {
+            return request($key . '.' . $locale);
         }
 
         return request($key);
@@ -72,7 +72,7 @@ trait Translation
 
     public function saveTranslation($key, $locale, $value): void
     {
-        cache('multilang')
+        Locale::multilang()
             ? $this->setTranslation($key, $locale, $value)
             : $this->{$key} = $value;
     }
