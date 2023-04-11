1df2cb29165ae309d21b39d83af8ccee0c896bef
Andrian Mihailov <amihailov@wagaia.com>
2023-04-11 18:14:08 +0200

    Update: 0.24.1 TranslatableFillablesTabs

--- src/Resources/views/components/translatable-fillables-tabs.blade.php
+++ src/Resources/views/components/translatable-fillables-tabs.blade.php
@@ -1,10 +1,13 @@
-@foreach(config('mfw.translatable.locales') as $locale)
-    <div class="tab-pane fade {!! $locale == app()->getLocale() ? 'show active': null !!}"
-         id="{{ $id }}_{{ $locale }}"
-         role="tabpanel"
-         aria-labelledby="{{ $id }}_btn_{{ $locale }}">
-        <div class="row mb-4">
-            <x-mfw::fillable-parser :datakey="$datakey" :fillables="$fillables" :model="$model" :locale="$locale" />
+<x-mfw::language-tabs id="{{ $id }}"/>
+<div class="tab-content pt-4">
+    @foreach(config('mfw.translatable.locales') as $locale)
+        <div class="tab-pane fade {!! $locale == app()->getLocale() ? 'show active': null !!}"
+             id="{{ $id }}_{{ $locale }}"
+             role="tabpanel"
+             aria-labelledby="{{ $id }}_btn_{{ $locale }}">
+            <div class="row mb-4">
+                <x-mfw::fillable-parser :datakey="$datakey" :fillables="$fillables" :model="$model" :locale="$locale"/>
+            </div>
         </div>
-    </div>
-@endforeach
+    @endforeach
+</div>
