f833ce06d3298b6da132d59b0b6877172970c935
Andrian Mihailov <amihailov@wagaia.com>
2023-04-27 11:14:33 +0200

    Update: 0.40.2 Modal

--- src/Resources/views/components/modal.blade.php
+++ src/Resources/views/components/modal.blade.php
@@ -17,7 +17,7 @@
                     </button>
                 </div>
                 <div class="modal-body">
-                    <p>{{ $question }}</p>
+                    <p>{!! $question !!}</p>
                 </div>
                 <div class="modal-footer">
                     <button type="button" class="btn btn-sm" data-bs-dismiss="modal" aria-hidden="true">{{ __('ui.cancel') }}</button>
