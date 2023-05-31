b04bbaced75aede90597b55add2320248f6d7d25
Andrian Mihailov <amihailov@wagaia.com>
2023-05-31 16:18:56 +0200

    Update: Simple Modal

---
+++ src/Components/SimpleModal.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace MetaFramework\Components;
+
+use Illuminate\Contracts\Support\Renderable;
+use Illuminate\View\Component;
+
+class SimpleModal extends Component
+{
+    /**
+     * @param string $text // Link text
+     * @param string $id // Modal ID
+     * @param string $title // Modal question
+     * @param string|null $body // Modal body text
+     * @param string|null $confirm // confirm btn text
+     * @param string|null $cancel // cancel btn text
+     * @param string $confirmclass // confirm btn class
+     */
+
+    public function __construct(
+        public string $text,
+        public string $id,
+        public string $title,
+        public ?int $modelid = null,
+        public ?string $identifier = null,
+        public ?string $class = null,
+        public ?string $confirm = null,
+        public ?string $callback = null,
+        public ?string $body = null,
+        public ?string $cancel = null,
+        public string $confirmclass = 'btn-success'
+    )
+    {
+        if (!$this->confirm) {
+            $this->confirm = "<i class='fa-solid fa-check'></i>" . __('mfw.confirm');
+        }
+        if (!$this->cancel) {
+            $this->cancel = __('mfw.cancel');
+        }
+    }
+
+    public function render(): Renderable
+    {
+        return view('mfw::components.simple-modal');
+    }
+}
---
+++ src/Resources/views/components/simple-modal.blade.php
@@ -0,0 +1,66 @@
+<a href="#"
+   data-model-id="{{ $modelid }}"
+   data-identifier="{{ $identifier }}"
+   class="{{ $class }}"
+   data-bs-toggle="modal"
+   data-bs-target="#mfw-simple-modal"
+   data-modal-id="{{ $id }}"
+   data-title="{!! $title !!}"
+   data-body="{!! $body !!}"
+   data-btn-confirm="{!! $confirm !!}"
+   data-callback="{{ $callback }}"
+   data-btn-confirm-class="{!! $confirmclass !!}"
+   data-btn-cancel="{!! $cancel !!}"
+>
+    {!! $text !!}
+</a>
+
+@pushonce('js')
+    <div class="modal fade" id="mfw-simple-modal" tabindex="-1" aria-labelledby="mfw-simple-modal_Label" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title"></h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="{{ __('ui.close') }}"></button>
+                </div>
+                <div class="modal-body"></div>
+                <div class="modal-footer d-flex justify-content-between">
+                    <button type="button" class="btn btn-secondary btn-cancel" data-bs-dismiss="modal"></button>
+                    <button type="button" class="btn btn-confirm"></button>
+                </div>
+            </div>
+        </div>
+    </div>
+    <script>
+      let mfwSimpleModal = new bootstrap.Modal(document.getElementById('mfw-simple-modal'));
+
+      $(document).ready(function () {
+
+        let jQuery_mfwSimpleModal = $('#mfw-simple-modal');
+
+        jQuery_mfwSimpleModal.off().on('show.bs.modal', function (event) {
+          let button = $(event.relatedTarget),
+            callback = button.data('callback');
+
+          jQuery_mfwSimpleModal.find('.modal-title').html(button.data('title')).end().
+          find('.modal-body').html(button.data('body')).end().
+          find('.btn-cancel').html(button.data('btn-cancel')).end().
+          find('.btn-confirm')
+            .addClass(button.data('btn-confirm-class'))
+            .addClass(button.data('modal-id'))
+            .attr('data-model-id', button.data('model-id'))
+            .attr('data-identifier', button.data('identifier'))
+            .html(button.data('btn-confirm'));
+
+          if (callback !== undefined && typeof window[callback] === 'function') {
+            window[callback]();
+          }
+
+        }).on('hide.bs.modal', function () {
+          jQuery_mfwSimpleModal.find('.modal-title, .modal-body, .btn-confirm, .btn-cancel').html('').end().
+          find('.btn-confirm').attr('class', 'btn btn-confirm').removeAttr('data-model-id').removeAttr('data-identifier');
+        });
+      });
+    </script>
+
+@endpushonce
