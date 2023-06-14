11d97228ba40615deebfa96cc8cd20266d36d142
Andrian Mihailov <amihailov@wagaia.com>
2023-06-14 21:04:58 +0200

    Update: ForceInteger cast

---
+++ src/Casts/ForceInteger.php
@@ -0,0 +1,28 @@
+<?php
+namespace MetaFramework\Casts;
+
+use DateTime;
+use Illuminate\Contracts\Database\Eloquent\CastsAttributes;
+
+
+class ForceInteger implements CastsAttributes
+{
+
+    /**
+     * @throws \Exception
+     */
+    public function get($model, $key, $value, $attributes)
+    {
+        return $this->toInteger($value);
+    }
+
+    public function set($model, $key, $value, $attributes)
+    {
+        return $this->toInteger($value);
+    }
+
+    private function toInteger($value): int
+    {
+        return is_numeric($value) ? intval($value) : 0;
+    }
+}
