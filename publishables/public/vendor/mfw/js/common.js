479d1090f7bd8332a6315cc38ecd65f54961e2bc
Andrian Mihailov <amihailov@wagaia.com>
2023-07-24 11:21:50 +0200

    Correction et ajout de fonctions JS de génération de clés

--- publishables/public/vendor/mfw/js/common.js
+++ publishables/public/vendor/mfw/js/common.js
@@ -120,7 +120,18 @@ let token = function () {
     return text.toString().toLowerCase().replace(/\s+/g, '-').replace(/[^\u0100-\uFFFF\w\-]/g, '-').replace(/\-\-+/g, '-').replace(/^-+/, '').replace(/-+$/, '');
   },
   guid = function (keylength = 9) {
-    return Math.random().toString(36).substr(2, keylength);
+    let str = '';
+    while (str.length < keylength) {
+      str += Math.random().toString(36).substr(2);
+    }
+    return str.substr(0, keylength);
+  },
+  generateUUID = function () {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+      var r = Math.random() * 16 | 0,
+        v = c === 'x' ? r : (r & 0x3 | 0x8);
+      return v.toString(16);
+    });
   },
   access_key = function (iteration = 10, keylength = 16) {
     function s4() {
