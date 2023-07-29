fda3ff1c83e7fcaf2a7c6df2fcaa9b7e208287ae
Andrian Mihailov <amihailov@wagaia.com>
2023-07-29 23:14:30 +0200

    Fix: InputDateMask component blade syntax

--- src/Resources/views/components/inputdatemask.blade.php
+++ src/Resources/views/components/inputdatemask.blade.php
@@ -1,4 +1,4 @@
 <x-mfw::input :name="$name" :value="$value" :label="$label" :class="$class" :required="$required" :params="$params" />
 @pushonce('js')
-<script src="{{ asset('vendor/mfw/components/inputdatemask.js') }}"
+<script src="{{ asset('vendor/mfw/components/inputdatemask.js') }}"></script>
 @endpushonce
