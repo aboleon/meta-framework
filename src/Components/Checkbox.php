20a7adbff85dd874384dafa578a2a255ae24862f
Andrian Mihailov <amihailov@wagaia.com>
2023-09-19 10:42:28 +0200

    Bugfix: unique id checkbox component

--- src/Components/Checkbox.php
+++ src/Components/Checkbox.php
@@ -4,6 +4,7 @@ namespace MetaFramework\Components;
 
 use Illuminate\Contracts\Support\Renderable;
 use Illuminate\Support\Collection;
+use Illuminate\Support\Str;
 use Illuminate\View\Component;
 use MetaFramework\Functions\Helpers;
 
@@ -13,20 +14,21 @@ class Checkbox extends Component
     public bool $isSelected = false;
 
     public function __construct(
-        public int | string | null $value,
-        public string $name,
-        public mixed $affected = null,
-        public string|null $label = '',
-        public string $class ='',
-        public bool $switch = false,
-        public array $params=[]
-    ) {
+        public int|string|null $value,
+        public string          $name,
+        public mixed           $affected = null,
+        public string|null     $label = '',
+        public string          $class = '',
+        public bool            $switch = false,
+        public array           $params = []
+    )
+    {
 
         if (!$this->affected instanceof Collection) {
             $this->affected = collect($this->affected);
         }
 
-        $this->forLabel = Helpers::generateInputId($this->name);
+        $this->forLabel = Helpers::generateInputId($this->name . Str::random(10));
         $this->isSelected = $this->affected->contains($this->value);
         $this->name = Helpers::generateInputName($this->name);
     }
