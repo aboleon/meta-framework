e829826506403151c5cfaa032053a52c819a3658
Andrian Mihailov <amihailov@wagaia.com>
2023-09-21 12:02:04 +0200

    NullablePriceInteger Cast

---
+++ src/Casts/NullablePriceInteger.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace MetaFramework\Casts;
+
+use Illuminate\Contracts\Database\Eloquent\CastsAttributes;
+
+
+class NullablePriceInteger implements CastsAttributes
+{
+
+    /**
+     * @throws \Exception
+     */
+    public function get($model, $key, $value, $attributes): ?int
+    {
+        return isset($value) ? $value/100 : null;
+    }
+
+    public function set($model, $key, $value, $attributes): ?int
+    {
+        return isset($value) ? $value*100 : null;
+    }
+}
