41204a40b5531aee31a77b3824593122dd0af189
Andrian Mihailov <amihailov@wagaia.com>
2023-09-22 16:53:00 +0200

    Fix: edit link rename

--- src/Resources/views/components/edit-link.blade.php
+++ src/Resources/views/components/edit-link.blade.php
@@ -1,4 +1,4 @@
 <li>
-    <a href="{{ $route }}" class="edit-link btn btn-sm btn-secondary" data-bs-toggle="tooltip" data-bs-placement="top" data-bs-title="{{ __('mfw.edit') }}">
+    <a href="{{ $route }}" class="mfw-edit-link btn btn-sm btn-secondary" data-bs-toggle="tooltip" data-bs-placement="top" data-bs-title="{{ __('mfw.edit') }}">
         <i class="fas fa-pen"></i></a>
 </li>
