c72be393ddbb901f606c39fdff75fb02ce01efc6
Andrian Mihailov <amihailov@wagaia.com>
2023-10-10 10:39:59 +0200

    Update: TinyMce

--- src/Resources/views/components/textarea.blade.php
+++ src/Resources/views/components/textarea.blade.php
@@ -15,4 +15,25 @@
     required
 @endif
 >{!! $value !!}</textarea>
+
 <x-mfw::validation-error :field="$validation_id"/>
+
+@if(str_contains($class,'simplified') or str_contains($class, 'extended'))
+    @pushonce('js')
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/tinymce/5.8.2/tinymce.min.js"></script>
+        <script id="tinymce_settings" src="{!! asset('vendor/mfw/js/tinymce/default_settings.js') !!}"></script>
+        <script>
+          if ($('textarea.extended').length) {
+            tinymce.init(mfw_default_tinymce_settings('textarea.extended'));
+          }
+          $(function() {
+            if ($('textarea.simplified').length) {
+              var url = "{!! asset('vendor/mfw/js/tinymce/simplified.js') !!}";
+              $.when($.getScript(url)).then(function() {
+                tinymce.init(mfw_simplified_tinymce_settings('textarea.simplified'));
+              });
+            }
+          });
+        </script>
+    @endpushonce
+@endif
