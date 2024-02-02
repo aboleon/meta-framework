199131cf6980e8df8a5a9192d4f2e6e6129c4f6e
Andrian Mihailov <amihailov@wagaia.com>
2024-02-02 16:57:31 +0100

    Update: Google component JS

--- publishables/public/vendor/mfw/components/google-places-geolocate.js
+++ publishables/public/vendor/mfw/components/google-places-geolocate.js
@@ -1,6 +1,7 @@
 function isGoogleMapsApiLoaded() {
-    return typeof google !== 'undefined' && typeof google.maps !== 'undefined';
+  return typeof google !== 'undefined' && typeof google.maps !== 'undefined';
 }
+
 function initialize() {
 
   // Bias the autocomplete object to the user's geographical location,
@@ -24,19 +25,19 @@ function initialize() {
   function setGoogleBar(element) {
 
     let mapsbar_id = element.attr('id'),
-      autocomplete,
-      componentForm = {
-        street_number: 'long_name',
-        route: 'long_name',
-        locality: 'long_name',
-        administrative_area_level_1: 'long_name',
-        administrative_area_level_2: 'long_name',
-        country: 'long_name',
-        postal_code: 'short_name',
-        regions: 'long_name',
-      },
-      google_places_params = $('#params_' + mapsbar_id),
-      api_options = {types: ['geocode']};
+        autocomplete,
+        componentForm = {
+          street_number: 'long_name',
+          route: 'long_name',
+          locality: 'long_name',
+          administrative_area_level_1: 'long_name',
+          administrative_area_level_2: 'long_name',
+          country: 'long_name',
+          postal_code: 'short_name',
+          regions: 'long_name',
+        },
+        google_places_params = $('#params_' + mapsbar_id),
+        api_options = {types: ['geocode']};
 
     if (google_places_params.length) {
       let options = $.trim(google_places_params.text());
@@ -49,18 +50,21 @@ function initialize() {
     autocomplete = new google.maps.places.Autocomplete(element.find('.g_autocomplete')[0], api_options);
 
     google.maps.event.addListener(autocomplete, 'place_changed', function () {
+
       // Get the place details from the autocomplete object.
       var place = autocomplete.getPlace();
       console.log(place);
+
+      // element.find('.lockable').prop('readonly', false);
+
       for (var component in componentForm) {
         element.find('.' + component).val('').prop('disabled', false);
-        /* document.getElementById(component).value = '';
-        document.getElementById(component).disabled = false; */
       }
+
       let country_code = element.find('.country_code').first();
       country_code.val('').prop('disabled', false);
-      //document.getElementById('country_code').disabled = false;
       let address = [];
+
       // Get each component of the address from the place details
       // and fill the corresponding field on the form.
       for (var i = 0; i < place.address_components.length; i++) {
@@ -78,10 +82,6 @@ function initialize() {
         }
       }
 
-
-      //setup address 1
-      element.find('.address1').val(address.join(', '));
-
       element.find('.wa_geo_lat').val(place.geometry.location.lat()).change();
       element.find('.wa_geo_lon').val(place.geometry.location.lng()).change();
 
@@ -91,6 +91,18 @@ function initialize() {
         if (typeof fn === 'function') {
           fn(element);
         }
+      }
+    });
+
+    element.find('.g_autocomplete').change(function () {
+      console.log('change in gautocomplete')
+      console.log($.trim($(this).val()).length);
+      if ($.trim($(this).val()).length > 0) {
+        console.log('unlocking');
+        element.find('.lockable').prop('readonly', false);
+      } else {
+        console.log('locking');
+        element.find('.lockable').prop('readonly', true);
       }
     });
 
