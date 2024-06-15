d10386f564e6b54c2f1ec4a8573de7f33f6da09a
Andrian Mihailov <amihailov@wagaia.com>
2024-06-15 16:33:42 +0200

    Responses log

--- src/Functions/functions.php
+++ src/Functions/functions.php
@@ -1,7 +1,8 @@
 <?php
-
-function mb_ucfirst(string $str): string
-{
-    $fc = mb_strtoupper(mb_substr($str, 0, 1));
-    return $fc . mb_substr($str, 1);
+if (!function_exists('mb_ucfirst')) {
+    function mb_ucfirst(string $str): string
+    {
+        $fc = mb_strtoupper(mb_substr($str, 0, 1));
+        return $fc . mb_substr($str, 1);
+    }
 }
