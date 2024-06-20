431d1df04a0c3afcf45110e2c4de3679cb03743d
Andrian Mihailov <amihailov@wagaia.com>
2024-06-20 14:50:36 +0200

    mb_ucfirst collision

--- src/Functions/functions.php
+++ src/Functions/functions.php
@@ -1,8 +1,6 @@
 <?php
-if (!function_exists('mb_ucfirst')) {
-    function mb_ucfirst(string $str): string
-    {
-        $fc = mb_strtoupper(mb_substr($str, 0, 1));
-        return $fc . mb_substr($str, 1);
-    }
+function mfw_ucfirst(string $str): string
+{
+    $fc = mb_strtoupper(mb_substr($str, 0, 1));
+    return $fc . mb_substr($str, 1);
 }
